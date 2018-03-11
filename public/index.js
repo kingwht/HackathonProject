37cd07a13bafde51d666d1bd89a4ddbd5b3367d6
Haotong Wang <wht972332930@163.com>
2018-03-10 16:42:25 -0800

    my files

---
+++ public/index.js
@@ -0,0 +1,15 @@
+$('.Origins').click(function(){
+    console.log("inside click show");
+    $('.Origin-pop-up img').show();
+    // $('.Origins').hide(0);
+});
+
+// (function() {
+//     var $origins = document.getElementsByClassName('Origins-pop-up img');
+//     $origins.className('fade-in');
+// })();
+// $('.pop-up span').click(function() {
+//     $('.pop-up-container').hide(0);
+//     $('.pop-up').hide(0);
+//     $('.pop-up-button').show(0);
+// });
